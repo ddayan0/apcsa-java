d3535a5a2621385d56d23f370974f2aa1f614208
ddayan0 <denday198@gmail.com>
2019-11-26 10:23:13 -0500

    added comments for arrayProj

--- arrayProj.java
+++ arrayProj.java
@@ -1,29 +1,29 @@
 // Dennis Dayan
 // AP CSA
 
-import java.util.*;
-import java.lang.*;
+import java.util.*; //imports java.util
+import java.lang.*; //imports java.lang
 
 public class arrayProj {
 
     static void wheeloffotune(){
-        String bankone [] = {"The Godfather", "Citizen Kane", "Casablanca", "Pulp Fiction", "Star Wars"};
-        String banktwo [] = {"Bugatti Veyron", "Toyota Prius", "Honda Civic", "Mclaren Senna", "Ferrari LaFerrari"};
-        String bankthree[] = {"Barack Obama", "Saddam Hussein", "Hillary Clinton", "Donald Trump", "Bernie Sanders"};
-        String bankfour [] = {"Think Different", "Just Do It", "Breakfast of Champions", "Can You Hear Me Now", "Got Milk"};
-        String cats [] = {"Movies", "Cars", "People", "Slogans"};
+        String bankone [] = {"The Godfather", "Citizen Kane", "Casablanca", "Pulp Fiction", "Star Wars"}; //Movies
+        String banktwo [] = {"Bugatti Veyron", "Toyota Prius", "Honda Civic", "Mclaren Senna", "Ferrari LaFerrari"};//Cars
+        String bankthree[] = {"Barack Obama", "Saddam Hussein", "Hillary Clinton", "Donald Trump", "Bernie Sanders"};//People
+        String bankfour [] = {"Think Different", "Just Do It", "Breakfast of Champions", "Can You Hear Me Now", "Got Milk"};//Slogans
+        String cats [] = {"Movies", "Cars", "People", "Slogans"}; // Array containing categories for the independent solve function
         // get a phrase and then cat from it
         //sout category, store phrase
-        int max = 3;
-        int min = 0;
-        int range = max - min + 1;
-        int rand = (int)(Math.random() * range) + min;
-        String phrase = " ";
-        String catstr = " ";
-        if(rand == 0){
-            phrase = bankone[rand];
-            catstr = cats[0];
-        }
+        int max = 3; //Max index for cats []
+        int min = 0; // Lowest index for cats []
+        int range = max - min + 1; //setting range for math.random in between max and min
+        int rand = (int)(Math.random() * range) + min; //Generates a number within that range and stores it into the int rand
+        String phrase = " "; // empty phrase for init
+        String catstr = " "; // empty category for init
+        if(rand == 0){ // if lowest index
+            phrase = bankone[rand]; // get random phrase from bankone
+            catstr = cats[0]; // phrase automatically Cars as showin in cats array
+        } // repeats until rand==3, no further explanation necessary
         if(rand == 1){
             phrase = banktwo[rand];
             catstr = cats[1];
@@ -36,24 +36,24 @@ public class arrayProj {
             phrase = bankfour[rand];
             catstr = cats[3];
         }
-        int points [] = {25, 50, 100, 150, 200};
-        int userpoints = 0;
+        int points [] = {25, 50, 100, 150, 200};// bank of points to be awarded to the user 
+        int userpoints = 0; // initializes userpoints with a value of zero
         System.out.println("Welcome to Wheel of Fortune!");
         System.out.println("Choose an Option");
-        System.out.println("1: Quit, 2: Spin, 3: Solve, 4: Print Points");
-        Scanner menub = new Scanner(System.in);
-        int option = menub.nextInt();
-        int tries = 0;
-        while(tries == 0) {
+        System.out.println("1: Quit, 2: Spin, 3: Solve, 4: Print Points"); // menu for user
+        Scanner menub = new Scanner(System.in); // initializes new scanner instance menub for the menu ONLY
+        int option = menub.nextInt(); // sets int menub to an option variable
+        int tries = 0; // Can remove  
+        while(tries == 0) { // Can be replaced with true, but this works as of now (11/26/19)
             if (option == 1) {
-                break;
+                break; // quits entire program
             }
-            if (option == 2) {
-                System.out.println("Your category is: Cars");
-                String phrase1 = "Bugatti Veyron";
-                String phrase1Arr [] = {"B", "u", "g", "a","t", "t", "i", " ", "V", "e", "y", "r", "o", "n"};
-                String phrase1Gs [] = {"_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"};
-                Scanner op2guess = new Scanner(System.in);
+            if (option == 2) { // SPIN function
+                System.out.println("Your category is: Cars"); // set category, DOES NOT USE cats[] ARRAY
+                String phrase1 = "Bugatti Veyron"; // set phrase, DOES NOT USE bank*[] ARRAY
+                String phrase1Arr [] = {"B", "u", "g", "a","t", "t", "i", " ", "V", "e", "y", "r", "o", "n"};//Seperates phrase into letters
+                String phrase1Gs [] = {"_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"}; // into dashes for display
+                Scanner op2guess = new Scanner(System.in); 
                 String userguess = op2guess.nextLine();
                 for(int i = 0; i < phrase1Arr.length; i++){
                     if(phrase1Arr[i].equalsIgnoreCase(userguess)){
