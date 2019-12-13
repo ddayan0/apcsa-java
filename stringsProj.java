45dbf7fecda0de67cee050ced013c9f8bd8b6d4f
Dennis D <denday198@gmail.com>
2019-12-13 12:48:47 -0500

    Create stringsProj.java

---
+++ stringsProj.java
@@ -0,0 +1,71 @@
+// Dennis Dayan
+// Mr. Watson
+// AP Comp Sci A
+// 13 December 2019 (so far)
+// stringsProj.java
+
+// the new age must come
+// java must be dead soon
+// we want our functions back
+
+// TODO Mini Proj #4 (12 pts)
+//        Format Name
+//        Convert a name in the format LAST, FIRST to FIRST LAST
+//        Make sure to handle spaces and apostrophes
+//        Eg: von Neumann, John => John von Neumann
+//        Eg: O'Reilly, Bill => Bill O'Reilly
+
+// TODO Mini Proj #6 (12 pts)
+//        How Many Multiples?
+//        Determine how many letters are found multiple times in a String passed as an argument
+//        Print to the console a single instance of a letter that is contained in a String two or more times
+//        Eg: “Hello World”
+//        l appears 3 times
+//        o appears 2 times
+//        If there are no multiples, print an error message to the user
+//
+
+// TODO Mini Proj #5 (12 pts)
+//        Reverse Each
+//        Reverse each word of a small paragraph provided as a parameter
+//        This must not reverse the entire paragraph
+//        You may use ONE method not on the Strings Slides, but it must be a String method (Choose wisely)
+//        Must handle punctuation, special characters, and spacing appropriately
+//        Eg: “The quick brown fox jumped over the lazy dog!”
+//        ehT kciuq nworb xof depmuj revo eht yza'l !god
+
+// ok first of all, why do we have to do all the hard ones for full credit?
+// i mean ok, i guess you want to challenge us and motivate us to challenge ourselves
+// but
+// thats just a little mean
+
+
+
+// Python > C > Java
+public class stringsProj {
+
+    static void line(){ //line function for organization
+        System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"); //ooooo fancy
+    } //end line()
+
+    static void formatName(String target){ //method for miniProj #4
+        
+    }//end formatName()
+
+    static void howManyMultiples(String target){ //method for miniProj #6
+
+    }//end howManyMultiples()
+
+    static void reverseEach(String target){ //method for miniProj #5
+
+    }//end reverseEach()
+
+    public static void main(String[] args) { //main method (obviously)
+        line(); // calls line function
+    } //end psvm()
+
+
+
+
+
+}
