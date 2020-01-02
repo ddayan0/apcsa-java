9cc4ddf7f3021cf50a5af9b7ab68a47f0dd75fc4
Dennis D <denday198@gmail.com>
2020-01-02 12:41:32 -0500

    updated without regex functionality
    MAKE COMMENTS AND README!

--- strings/stringsProj.java
+++ strings/stringsProj.java
@@ -44,8 +44,8 @@
 // thats just a little mean
 
 
-import java.util.Arrays; //for array functionality
-import java.util.regex.*; // regex for HowManyMultiples
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
 
 // Python > C > Java
 public class stringsProj {
@@ -62,338 +62,43 @@ public class stringsProj {
     }//end formatName()
 
     static void howManyMultiples(String target){ //method for miniProj #6
-	    	String newTarg = target.toLowerCase(); // sets user supplied target to lowercase for ease
-		Pattern a = Pattern.compile("a"); // compiles a pattern for the regex to loook for
-		Matcher amatch = a.matcher(newTarg); // creates matcher object
-		int acount = 0; // inits count of letters to zero
-		while (amatch.find()){ // while the matcher object is looking for the pattern 
-			acount++; // add 1 to count of letters if it finds it
-		}
-		if(acount <= 1){ // if the count is less than or equal to one
-			System.out.println(""); // just print a blank
-		}
-		else{
-			System.out.println("A is found " + acount + " Times"); //prints count THIS IS DONE FOR THE REST OF THE ALPHABET
-		}
-		Pattern b = Pattern.compile("b");
-		Matcher bmatch = b.matcher(newTarg);
-		int bcount = 0;
-		while (bmatch.find()){
-			bcount++;
-		}
-		if(bcount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("B is found " + bcount + " Times");
-		}
-		Pattern c = Pattern.compile("c");
-		Matcher cmatch = c.matcher(newTarg);
-		int ccount = 0;
-		while (cmatch.find()){
-			ccount++;
-		}
-		if(ccount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("C is found " + ccount + " Times");
-		}
-		Pattern d = Pattern.compile("d");
-		Matcher dmatch = d.matcher(newTarg);
-		int dcount = 0;
-		while (dmatch.find()){
-			dcount++;
-		}
-		if(dcount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("D is found " + dcount + " Times");
-		}
-		Pattern e = Pattern.compile("e");
-		Matcher ematch = e.matcher(newTarg);
-		int ecount = 0;
-		while (ematch.find()){
-			ecount++;
-		}
-		if(ecount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("E is found " + ecount + " Times");
-		}
-		Pattern f = Pattern.compile("f");
-		Matcher fmatch = f.matcher(newTarg);
-		int fcount = 0;
-		while (fmatch.find()){
-			fcount++;
-		}
-		if(fcount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("F is found " + fcount + " Times");
-		}
-		Pattern g = Pattern.compile("g");
-		Matcher gmatch = g.matcher(newTarg);
-		int gcount = 0;
-		while (gmatch.find()){
-			gcount++;
-		}
-		if(gcount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("G is found " + gcount + " Times");
-		}
-		Pattern h = Pattern.compile("h");
-		Matcher hmatch = h.matcher(newTarg);
-		int hcount = 0;
-		while (hmatch.find()){
-			hcount++;
-		}
-		if(hcount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("H is found " + hcount + " Times");
-		}
-		Pattern i = Pattern.compile("i");
-		Matcher imatch = i.matcher(newTarg);
-		int icount = 0;
-		while (imatch.find()){
-			icount++;
-		}
-		if(icount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("I is found " + icount + " Times");
-		}
-		Pattern j = Pattern.compile("j");
-		Matcher jmatch = j.matcher(newTarg);
-		int jcount = 0;
-		while (jmatch.find()){
-			jcount++;
-		}
-		if(jcount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("J is found " + jcount + " Times");
-		}
-		Pattern k = Pattern.compile("k");
-		Matcher kmatch = k.matcher(newTarg);
-		int kcount = 0;
-		while (kmatch.find()){
-			kcount++;
-		}
-		if(kcount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("K is found " + kcount + " Times");
-		}
-		Pattern l = Pattern.compile("l");
-		Matcher lmatch = l.matcher(newTarg);
-		int lcount = 0;
-		while (lmatch.find()){
-			lcount++;
-		}
-		if(lcount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("L is found " + lcount + " Times");
-		}
-		Pattern m = Pattern.compile("m");
-		Matcher mmatch = m.matcher(newTarg);
-		int mcount = 0;
-		while (mmatch.find()){
-			mcount++;
-		}
-		if(mcount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("M is found " + mcount + " Times");
-		}
-		Pattern n = Pattern.compile("n");
-		Matcher nmatch = n.matcher(newTarg);
-		int ncount = 0;
-		while (nmatch.find()){
-			ncount++;
-		}
-		if(ncount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("N is found " + ncount + " Times");
-		}
-		Pattern o = Pattern.compile("o");
-		Matcher omatch = o.matcher(newTarg);
-		int ocount = 0;
-		while (omatch.find()){
-			ocount++;
-		}
-		if(ocount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("O is found " + ocount + " Times");
-		}
-		Pattern p = Pattern.compile("p");
-		Matcher pmatch = p.matcher(target);
-		int pcount = 0;
-		while (pmatch.find()){
-			pcount++;
-		}
-		if(pcount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("P is found " + pcount + " Times");
-		}
-		Pattern q = Pattern.compile("q");
-		Matcher qmatch = q.matcher(newTarg);
-		int qcount = 0;
-		while (qmatch.find()){
-			qcount++;
-		}
-		if(qcount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("Q is found " + qcount + " Times");
-		}
-		Pattern r = Pattern.compile("r");
-		Matcher rmatch = r.matcher(newTarg);
-		int rcount = 0;
-		while (rmatch.find()){
-			rcount++;
-		}
-		if(rcount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("R is found " + rcount + " Times");
-		}
-		Pattern s = Pattern.compile("s");
-		Matcher smatch = s.matcher(newTarg);
-		int scount = 0;
-		while (smatch.find()){
-			scount++;
-		}
-		if(scount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("S is found " + scount + " Times");
-		}
-		Pattern t = Pattern.compile("t");
-		Matcher tmatch = t.matcher(newTarg);
-		int tcount = 0;
-		while (tmatch.find()){
-			tcount++;
-		}
-		if(tcount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("T is found " + tcount + " Times");
-		}
-		Pattern u = Pattern.compile("u");
-		Matcher umatch = u.matcher(newTarg);
-		int ucount = 0;
-		while (umatch.find()){
-			ucount++;
-		}
-		if(ucount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("U is found " + ucount + " Times");
-		}
-		Pattern v = Pattern.compile("v");
-		Matcher vmatch = v.matcher(newTarg);
-		int vcount = 0;
-		while (vmatch.find()){
-			vcount++;
-		}
-		if(vcount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("V is found " + vcount + " Times");
-		}
-		Pattern w = Pattern.compile("w");
-		Matcher wmatch = w.matcher(newTarg);
-		int wcount = 0;
-		while (wmatch.find()){
-			wcount++;
-		}
-		if(wcount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("W is found " + wcount + " Times");
-		}
-		Pattern x = Pattern.compile("x");
-		Matcher xmatch = x.matcher(newTarg);
-		int xcount = 0;
-		while (xmatch.find()){
-			xcount++;
-		}
-		if(xcount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("x is found " + xcount + " Times");
-		}
-		Pattern y = Pattern.compile("y");
-		Matcher ymatch = y.matcher(newTarg);
-		int ycount = 0;
-		while (ymatch.find()){
-			ycount++;
-		}
-		if(ycount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("Y is found " + ycount + " Times");
-		}
-		Pattern z = Pattern.compile("z");
-		Matcher zmatch = z.matcher(newTarg);
-		int zcount = 0;
-		while (zmatch.find()){
-			zcount++;
-		}
-		if(zcount <= 1){
-			System.out.println("");
-		}
-		else{
-			System.out.println("Z is found " + zcount + " Times");
-		}
+        // AAAAHHHHHHHH WHY MUST THIS BE!
+        String comptarg = target.toLowerCase();
+        int strlen = comptarg.length();
+        int diff =0;
+        for(int i = 0; i < strlen; i++){
+            diff = removeLetter(target, target.substring(i, i+1)).length();
+            if((strlen - diff)!= 1){
+                System.out.println(target.substring(i, i+1)+" Repeats "+ (strlen - diff) + " times.");
+            }
+        }
     }//end howManyMultiples()
 
+    static String  removeLetter(String target, String toRemove){
+        String newStr = "";
+        for(int i = 0; i < target.length(); i++){
+            newStr = target.replace(toRemove, "");
+        }
+        return newStr;
+
+    }
     static void reverseEach(String target){ //method for miniProj #5
-	String[] words = target.split(" "); //splits spaces between each word in the sentence to reverse
-	String revStr = ""; // inits empty string to be filled later
-	for (int i = 0; i < words.length; i++){ //for:
-		String word = words[i]; // sets string object of word to index of string array words for the reversal
-		String revWrd = ""; // inits empty string to be filled later
-		for(int x = word.length()-1; x >= 0; x--){ //for:
-			revWrd = revWrd + word.charAt(x); // sets reverse word to the emp string + the string array at char index of x
-		}
-		revStr = revStr + revWrd + " "; // actually sets reverse string
-		int targlen = target.length()+1; //gets length of target string
-		int strlen = revStr.length(); // gets length of the reverse string
-		if(targlen == strlen){ // if they both equal
-			System.out.println(revStr); // then only print out the final reversed string
-		
-		}		
-	    }
+        String[] words = target.split(" "); //splits spaces between each word in the sentence to reverse
+        String revStr = ""; // inits empty string to be filled later
+        for (int i = 0; i < words.length; i++){ //for:
+            String word = words[i]; // sets string object of word to index of string array words for the reversal
+            String revWrd = ""; // inits empty string to be filled later
+            for(int x = word.length()-1; x >= 0; x--){ //for:
+                revWrd = revWrd + word.charAt(x); // sets reverse word to the emp string + the string array at char index of x
+            }
+            revStr = revStr + revWrd + " "; // actually sets reverse string
+            int targlen = target.length()+1; //gets length of target string
+            int strlen = revStr.length(); // gets length of the reverse string
+            if(targlen == strlen){ // if they both equal
+                System.out.println(revStr); // then only print out the final reversed string
+
+            }
+        }
     }//end reverseEach()
 
     //below are arrays useful to the program itself, these are not made to satisfy specific requirements
@@ -408,9 +113,9 @@ public class stringsProj {
         formatName("O'Reilly, Bill"); //formatName function
         line();
         howManyMultiples("Impossible"); // howManyMultiples
-	line();
-	reverseEach("The quick brown fox jumped over the lazy dog!"); //reverseEach
-	line();
+        line();
+        reverseEach("The quick brown fox jumped over the lazy dog!"); //reverseEach
+        line();
 
     } //end psvm()
 
